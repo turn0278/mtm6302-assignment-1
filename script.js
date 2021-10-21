a0aa4a7162da0c863952623a5c05f9d74b6be2b9
Shaun Turner <turn0278@algonquinlive.com>
2021-10-21 11:32:56 -0400

    adding comments becuase I forgot them

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 //Code
 const $library = document.getElementById('container')
 
+    // Setting a place to start in the emoji database with 100 emotjis in a row no blanks. In order to run a loop printing them out.
 const $emoji = [127744]                             
 
 
 //Functions
+
+    // x will run 100 times
 for (let x = 0; x < 100; x++) {
  $library.innerHTML += 
+    
+    // The iterator will be added by x adn then the emoji printed followed by the number beside the emoji
  `
  <p>&#${$emoji[0] + x};
  <code>${$emoji[0] + x}</code></p>
  `   
 
+    // It took far too long to debug this code for a simple "[]"
 }
